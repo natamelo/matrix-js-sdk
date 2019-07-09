39e33a8ccf4bbe41d358dba00689097c26b4c9fb
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-07-09 16:32:35 -0300

    Insert solicitation number on solicitation event

--- src/models/event-timeline-set.js
+++ src/models/event-timeline-set.js
@@ -485,9 +485,11 @@ EventTimelineSet.prototype.addLiveEvent = function(event, duplicateStrategy) {
                 if (isTheOldEventToSolicitation) {
 
                     if (!tlEvents[j].encryptedType) {
-                        tlEvents[j].getContent().status = 'Ciente';
-                        tlEvents[j].getContent().open_solicitation = false;
-                        this.realoadLocalTimeline(tlEvents[j], tlEvents[j].event.room_id, tlEvents[j].getId());
+                        // tlEvents[j].getContent().status = 'Ciente';
+                        // tlEvents[j].getContent().open_solicitation = false;
+                        // this.realoadLocalTimeline(tlEvents[j], tlEvents[j].event.room_id, tlEvents[j].getId());
+                        event.getContent().solicitation_number = tlEvents[j].getContent().solicitation_number;
+                        this.addEventToTimeline(event, this._liveTimeline, false);
                         break;
                     }
                 }
